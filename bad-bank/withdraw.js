f6348393553f530924407592de757bb995d2ddad
Caitlin Riddle <106290032+cqriddle@users.noreply.github.com>
2023-02-15 12:40:36 -0600

    Update withdraw.js

--- bad-bank/withdraw.js
+++ bad-bank/withdraw.js
@@ -3,38 +3,62 @@ function Withdraw() {
     const [show, setShow] = React.useState(true);
     const [withdraw, setWithdraw] = React.useState('');
     const [balance, setBalance] = React.useState(0);
+    const [validTransaction, setValidTransaction] = React.useState(false);
 
- 
-    
-        let transactionState = 0; // state of this transaction
-        let status = `Account Balance $ ${balance}`;
+    let transactionState = 0; // state of this transaction
+    // let status = `Account Balance $ ${balance}`;
+    let status1 = `Account Balance $ ${balance}`;
         
-        const handleChange = event => {
-          console.log(`handleChange ${event.target.value}`);
-          transactionState = Number(event.target.value);
-        };
-        const handleSubmit = () => {
-          setBalance(balance + transactionState);
-          event.preventDefault();
-          ctx.users.push({balance});
-        setShow(false);
-        };    
+    function handleCreate(){
+      if (withdraw > totalState) {
+       alert('Insufficient Funds');
+     } else {
+         setShow(false);
+       }}  
+ 
+ const handleChange = event => {
+   console.log(`handleChange ${event.target.value}`);
+   if (Number(event.target.value) > totalState) {
+     return setValidTransaction(false);
+   }
+   if (withdraw && Number(event.target.value) > totalState) {
+     setValidTransaction(false);
+   } else {
+     setValidTransaction(true);
+   }
+   setWithdraw(Number(event.target.value));
+ };
     
+    
+        const handleSubmit = () => {
+          // setBalance(balance + transactionState);
+          setBalance(Number(balance) - Number(withdraw))
+          event.preventDefault();
+          setWithdraw('');
+        setShow(true);
+        }; 
+
       function clearForm(){
         setWithdraw('');
         setShow(true);
+      }
+
+      function finalSubmit(){
+        handleCreate();
+        handleSubmit();
+        
       }
 
       return (
         <Card
         bgcolor="primary"
         header="Withdraw"
-        status={status}
+        status={status1}
         body={show ? (  
             <>
             Withdraw<br/>
             <input type="number" className="form-control" id="withdraw" placeholder="Withdraw Amount" value={withdraw} onChange={handleChange}/><br/>
-            <button type="submit" className="btn btn-light" onClick={handleSubmit}>Submit</button>
+            <button type="submit" className="btn btn-light" onClick={finalSubmit}>Submit</button>
             </>
           ):(
             <>
@@ -45,4 +69,6 @@ function Withdraw() {
   />
 )
 }
+
+
 
